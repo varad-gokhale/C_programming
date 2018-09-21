3b27c83a299ba50077a519e4e5f0dc1313fe7a72
Varad Gokhale <varad.gokhale91@gmail.com>
2018-09-21 11:50:14 -0500

    works

--- C_programming/recursive_atoi/atoi.c
+++ C_programming/recursive_atoi/atoi.c
@@ -1,19 +1,27 @@
 #include <stdio.h>
+#include <stdlib.h>
 
-int atoi_fn(char* s, int *mult, int *result)
+int atoi_fn(char* s, int *mult)
 {
-	if(s[i+1] == '\0'){
+	if(*(s+1) == '\0'){
 		return *s - '0';
 	}
 	
-	int digit = atoi_fn(s+1, mult, result);
-	*result += digit * (*mult);
+	int result = atoi_fn(s+1, mult);	
 	*mult *= 10;
-	return result;
+	return result + (*s - '0') * (*mult);
 }
 
-int main(int argc, int** argv){
+int main(int argc, char** argv){
 	int mult = 1;
-	int result = 0;
+	
+	if(argc < 2){
+		printf("Please enter the string to be converted\nExiting..\n");
+		exit(1);
+	}
+	printf("Number is: %d ", atoi_fn(argv[1], &mult));
+	printf("\n");
+	exit(0);
+}
 	
 	

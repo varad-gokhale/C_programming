2f8af22e058a40e2f4ea371ef4749b80ec816c33
Varad Gokhale <varad.gokhale91@gmail.com>
2018-09-30 14:49:06 -0500

    Works

--- C_programming/delete_kth_node_LL/delete_kth_node.c
+++ C_programming/delete_kth_node_LL/delete_kth_node.c
@@ -8,32 +8,57 @@ struct ListNode{
 
 void delete_kth_node(struct ListNode* head, int k) 
 {
-	if(k <= 0)	return;
+	if(k <= 1)	return;
 	delete_kth_node(head->next, k-1);
-	if(k == 1){
+	if(k == 2){
 		struct ListNode* del = head->next;
 		head->next = head->next->next;
 		free(del);
 	}
 }
 
-int main(int argc, char** argv){	
-	if(argc < 2){
-		printf("k not provided..Exiting");
-		exit(1);
-	}
-	if(atoi(argv[1]) > 7){
-		printf("Illegal argument provided. Please input value less than or equal to 7..exiting");
-		exit(1);
-	}
-	struct ListNode ll1,ll2,ll3,ll4,ll5,ll6,ll7;
-	l1 = {1,l2};
-	l2 = {2, l3};
-	l3 = {
-	
-	printf("Number is: %d ", atoi_fn(argv[1], &mult));
-	printf("\n");
-	exit(0);
-}
-	
-	
+/*Driver and util functions from www.geeksforgeeks.org/recursive-function-delete-k-th=node-linked-list/*/
+/* Utility function to insert a node at the beginning */
+void push(struct ListNode **head_ref, int new_data) 
+{ 
+    struct ListNode *new_node = (struct ListNode*)malloc(sizeof(struct ListNode)); 
+    new_node->val = new_data; 
+    new_node->next = *head_ref; 
+    *head_ref = new_node; 
+} 
+   
+/* Utility function to print a linked list */
+void printList(struct ListNode *head) 
+{ 
+    while (head!=NULL) 
+    {
+		printf("%d ",head->val); 
+        head = head->next; 
+    } 
+    printf("\n"); 
+} 
+   
+/* Driver program to test above functions */
+int main() 
+{ 
+    struct ListNode *head = NULL; 
+   
+    /* Create following linked list 
+      12->15->10->11->5->6->2->3 */
+    push(&head,3); 
+    push(&head,2); 
+    push(&head,6); 
+    push(&head,5); 
+    push(&head,11); 
+    push(&head,10); 
+    push(&head,15); 
+    push(&head,12); 
+    
+    int k = 4; 
+    delete_kth_node(head, k); 
+   
+    printf("\nModified Linked List: "); 
+    printList(head); 
+   
+    return 0; 
+} 

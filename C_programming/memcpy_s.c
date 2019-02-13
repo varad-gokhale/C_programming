73fd7b61146cf532a13e5edec690ac74fa89e9da
Varad Gokhale <varad.gokhale91@gmail.com>
2019-02-13 17:34:17 -0600

    Update memcpy_s.c
    added wrappers around thread APIs

--- C_programming/memcpy_s.c
+++ C_programming/memcpy_s.c
@@ -7,7 +7,8 @@ typedef enum{
   NO_ERROR = 0,
   ERROR_LOW_MEMORY,
   ERROR_STACK_FULL,
-  ERROR_STACK_EMPTY
+  ERROR_STACK_EMPTY,
+  ERROR_THREAD_FAIL
 }error_t;
 
 typedef struct{
@@ -27,52 +28,73 @@ stack_t* create_stack(int capacity, error_t *err)
   s -> top = 0;
   s -> arr = (int*)malloc(sizeof(int)*capacity);
   
-  pthread_mutex_init(&s->lock, NULL);
+  if(pthread_mutex_init(&s->lock, NULL) != 0)
+  {  
+    *err = ERROR_THREAD_FAIL;
+    return NULL;
+  }
   
+  *err = NO_ERROR;
   return s;
+}
+
+//wrapper function for pthread_mutex_lock
+void Pthread_mutex_lock(pthread_mutex_t *lock){
+  assert(pthread_mutex_lock(lock) == 0);  
+}
+
+//wrapper function for pthread_mutex_lock
+void Pthread_mutex_unlock(pthread_mutex_t *lock){
+  assert(pthread_mutex_unlock(lock) == 0);  
 }
 
 void push(stack_t* s, int a, error_t *err)
 {
-  pthread_mutex_lock(&s->lock);
+  Pthread_mutex_lock(&s->lock);
   
   if(s->top == s->capacity){
-    *err = ERROR_STACK_FULL;
-    pthread_mutex_unlock(&s->lock);
+    *err = ERROR_STACK_FULL;    
+    Pthread_mutex_unlock(&s->lock);    
     return;
   }
   s->arr[s->top++] = a;
   *err = NO_ERROR;
   
-  pthread_mutex_unlock(&s->lock);
+  Pthread_mutex_unlock(&s->lock);    
 }
 
 int pop(stack_t* s, error_t* err){
-  pthread_mutex_lock(&s->lock);
+  Pthread_mutex_lock(&s->lock);
   
   if(s->top == 0){
     *err = ERROR_STACK_EMPTY;
-    pthread_mutex_unlock(&s->lock);
+    Pthread_mutex_unlock(&s->lock);    
     return 0;
   }
   s->top -= 1;
   *err = NO_ERROR;
   
-  pthread_mutex_unlock(&s->lock);
+  Pthread_mutex_unlock(&s->lock);  
   
   return s->arr[s->top];
 }
 
-void free_stack(stack_t *s){
+void free_stack(stack_t *s, error_t* err){
   free(s->arr);
-  pthread_mutex_destroy(&s->lock);
+  if(pthread_mutex_destroy(&s->lock) != 0){
+    *err = ERROR_THREAD_FAIL;
+    return;
+  }
   free(s);
+  *err = NO_ERROR;
 }
 // To execute C, please define "int main()"
 
 int main() {
   error_t err = NO_ERROR;
+  
   stack_t* s = create_stack(3, &err);
+  assert(err == NO_ERROR);
   
   push(s, 1, &err);
   assert(err == NO_ERROR);
@@ -114,7 +136,8 @@ int main() {
   assert(err == ERROR_STACK_EMPTY);
   
   
-  free_stack(s);
+  free_stack(s, &err);
+  assert(err == NO_ERROR);
   
   return 0;
 }
